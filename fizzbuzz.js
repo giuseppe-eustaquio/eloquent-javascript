85fda2d4b3065ae0bc7663034bbb039e928382e4
Giuseppe Eustaquio <gseppe222@gmail.com>
2024-06-24 22:09:22 +0800

    adds fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+const isDivisibleBy = function (x) {
+  return (y) => y % x === 0;
+};
+const isDivisibleBy3 = isDivisibleBy(3);
+const isDivisibleBy5 = isDivisibleBy(5);
+
+for (let i = 1; i <= 100; i++) {
+  if (isDivisibleBy3(i)) {
+    process.stdout.write("Fizz");
+  }
+  if (isDivisibleBy5(i)) {
+    process.stdout.write("Buzz");
+  }
+
+  if (!(isDivisibleBy3(i) || isDivisibleBy5(i))) {
+    process.stdout.write(String(i));
+  }
+  console.log();
+}
