563509fd3311ef11c6b68d825034ea4306afd4ac
Giuseppe Eustaquio <gseppe222@gmail.com>
2024-07-09 22:31:20 +0800

    refactors exercises in list.js, rangesum.js, and reverse.js

--- list.js
+++ list.js
@@ -9,43 +9,69 @@ let sampleList = {
   },
 };
 
+// const prepend = function (list, newElement) {
+//   list = {
+//     value: newElement,
+//     rest: { ...list },
+//   };
+//   return list;
+// };
+
 const prepend = function (list, newElement) {
-  list = {
+  return {
     value: newElement,
-    rest: { ...list },
+    rest: list,
   };
-  return list;
 };
 
 const nth = function (list, number) {
   if (number === 0) {
-    return list["value"];
-  } else if (list["rest"] === null) {
+    return list.value;
+  } else if (list.rest === null) {
     return null;
   } else {
-    return nth(list["rest"], number - 1);
+    return nth(list.rest, number - 1);
   }
 };
 
+// const arrayToList = function (array) {
+//   let emptyList = { rest: null };
+//   array.forEach((element) => {
+//     emptyList = prepend(emptyList, element);
+//   });
+//   return emptyList;
+// };
+
 const arrayToList = function (array) {
-  let emptyList = { rest: null };
-  array.forEach((element) => {
-    emptyList = prepend(emptyList, element);
-  });
-  return emptyList;
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = prepend(list, array[i]);
+  }
+  return list;
 };
+
+// const listToArray = function (list) {
+//   const newArr = [];
+
+//   do {
+//     newArr.push(list["value"]);
+//     list = list["rest"];
+//   } while (list["rest"] != null);
+//   newArr.push(list["value"]);
+//   return newArr;
+// };
 
 const listToArray = function (list) {
   const newArr = [];
 
-  do {
-    newArr.push(list["value"]);
-    list = list["rest"];
-  } while (list["rest"] != null);
-  newArr.push(list["value"]);
+  while (list !== null) {
+    newArr.push(list.value);
+    list = list.rest;
+  }
+
   return newArr;
 };
 
-// console.log(arrayToList([1, 2, 3]));
-// console.log(nth(sampleList, 24));
-// console.log(listToArray(sampleList));
+console.log(arrayToList([1, 2, 3]));
+console.log(nth(sampleList, 2));
+console.log(listToArray(sampleList));
--- reverse.js
+++ reverse.js
@@ -1,21 +1,42 @@
+// const reverseArray = function (array) {
+//   const newArr = [];
+//   for (let i = 0; i < array.length; i++) {
+//     newArr.unshift(array[i]);
+//   }
+//   return newArr;
+// };
+const arrayTest = [2, 5, 6, 9, 4, 7];
+
 const reverseArray = function (array) {
-  const newArr = [];
+  const newArr = new Array(array.length);
   for (let i = 0; i < array.length; i++) {
-    newArr.unshift(array[i]);
+    newArr[array.length - 1 - i] = array[i];
   }
   return newArr;
 };
-const arrayTest = [2, 5, 6, 9, 4, 7];
 
 // console.log(reverseArray(arrayTest));
 // console.log(arrayTest);
 
+// const reverseArrayInPlace = function (array) {
+//   const oldArr = array.slice();
+//   for (let i = 0; i < array.length; i++) {
+//     array.unshift(oldArr[i]);
+//     array.pop();
+//   }
+//   return array;
+// };
+
 const reverseArrayInPlace = function (array) {
-  const oldArr = array.slice();
-  for (let i = 0; i < array.length; i++) {
-    array.unshift(oldArr[i]);
-    array.pop();
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    [array[left], array[right]] = [array[right], array[left]];
+    left++;
+    right--;
   }
+
   return array;
 };
 
